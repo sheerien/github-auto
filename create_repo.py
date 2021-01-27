1102956e4c841ee573ab95b7890a608b90f5cb6e
Shaaams <shamsssherien@gmail.com>
2021-01-27 21:34:29 +0300

    init commit

---
+++ create_repo.py
@@ -0,0 +1,33 @@
+import requests
+from pprint import pprint
+from secrets import GITHUB_TOKEN
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--name", "-n", type=str, dest="name", required=True)
+parser.add_argument("--private", "-p", dest="is_private", action="store_true")
+args = parser.parse_args()
+print(args)
+
+repo_name = args.name
+is_private = args.is_private
+
+API_URL = "https://api.github.com"
+
+if is_private:
+    payload = '{"name": "' + repo_name + '", "private":true}'
+else:
+    payload = '{"name": "' + repo_name + '", "private":false}'
+
+print(payload)
+
+headers = {
+    "Authorization": "token " + GITHUB_TOKEN,
+    "Accept": "application/vnd.github.v3+json"
+}
+try:
+    r = requests.post(API_URL+"/user/repos", data=payload, headers=headers)
+    r.raise_for_status()
+    # pprint(r.json())
+except requests.exceptions.RequestException as err:
+    raise SystemExit(err)
